55f8e98107fa8e0a739854b37d9750ff1f3429c0
dedalusmax <dedalus123@gmail.com>
2018-01-19 14:07:39 +0100

    Add project files.

---
+++ DataAccess/Data/ICourseRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4
+{
+    public interface ICourseRepository : IRepository<Course>
+    {
+        List<Course> GetTopSellingCourses();
+
+        List<Course> GetCoursesWithAuthors();
+    }
+}
---
+++ WebApplication4/Data/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4
+{
+    public interface IRepository<TEntity> where TEntity: class
+    {
+        void Add(TEntity entity);
+
+        void Remove(TEntity entity);
+
+        TEntity Get(Guid id);
+
+        TEntity Find(Predicate<TEntity> search);
+    }
+}
